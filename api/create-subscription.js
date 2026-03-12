ec3f8be9ea78abcf2ce42ef092fde782b81e0163
asherrobinsonfitness <asherapple2@gmail.com>
2026-03-12 07:02:04 -0400

    Update create-subscription.js

--- api/create-subscription.js
+++ api/create-subscription.js
@@ -1,15 +1,21 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-    const { customerId, paymentMethodId } = req.body;
+    const { customerId, paymentMethodId, email, name, phone } = req.body;
 
     await stripe.paymentMethods.attach(paymentMethodId, {
         customer: customerId,
     });
 
-    await stripe.customers.update(customerId, {
+    const customerUpdate = {
         invoice_settings: { default_payment_method: paymentMethodId },
-    });
+    };
+
+    if (email) customerUpdate.email = email;
+    if (name) customerUpdate.name = name;
+    if (phone) customerUpdate.phone = phone;
+
+    await stripe.customers.update(customerId, customerUpdate);
 
     const subscription = await stripe.subscriptions.create({
         customer: customerId,
